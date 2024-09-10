e64da2fde74f56a5faac4ba0ff75c8d849ad7ab4
priya2201 <priyamurpani99@gmailcom>
2024-09-10 11:02:10 +0530

    graph fix

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -71,7 +71,7 @@ export class FormComponent implements OnInit {
 
     const chartOptions = {
       chart: {
-        type: 'column'
+        type: 'formValues.chartType'
       },
       title: {
         text: 'Monthly Revenue'
